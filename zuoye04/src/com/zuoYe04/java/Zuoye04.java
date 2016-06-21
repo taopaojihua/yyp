206cc980fd80b0445feae5254e1e0810eba4a0fd
taopaojihua <1845530607@qq.com>
2016-06-21 13:36:16 +0800

    GitCode

---
+++ zuoye04/src/com/zuoYe04/java/Zuoye04.java
@@ -0,0 +1,12 @@
+package com.zuoYe04.java;
+
+public class Zuoye04 {
+	public static void main(String[] args) {
+		int[] a={1,2,3,4};//初始化数组
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);//循环输出
+		}
+		
+	}
+
+}
---
+++ zuoye04/src/com/zuoYe04/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.zuoYe04.java;
